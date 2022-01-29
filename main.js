fbdcf461580f023ec919c19eb4f38d86cdf0edd7
dazkharris <87241453+dazkharris@users.noreply.github.com>
2022-01-29 17:31:10 +0000

    Add files via upload

---
+++ main.js
@@ -0,0 +1,35 @@
+song = "";
+
+function preload() {
+    song = loadSound("music.mp3");
+}
+
+scoreRightWrist = 0;
+scoreLeftWrist = 0;
+
+rightWristX = 0;
+rightWristY = 0;
+
+leftWristX = 0;
+leftWristY = 0;
+
+function setup() {
+    canvas = createCanvas(600, 500);
+    canvas.center();
+
+    video = createCapture(VIDEO);
+    video.hide();
+
+    poseNet = ml5.poseNet(video, modelLoaded);
+    poseNet.on('pose', gotPoses);
+}
+
+function modelLoaded() {
+    console.log('PoseNet Is Initialized!');
+}
+
+function gotPoses(results) {
+    if (results.length > 0) {
+        console.log(results);
+    }
+}
